a64f54a5fb2bbba8d9424ae997b888380bf95c9a
beatriz.costa.ter <beatriz.costa.ter@estacio.br>
2021-08-17 18:40:15 -0300

    Adicionando o GKE via terraform

---
+++ terraform/modules/kubernetes/variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+    dafault =  ""
+}
+variable "application_name" {
+    default = "cluster-gcp"
+}
+variable "region" {
+    default = "us-central1"
+}
+variable "network_self_link" {
+    default = ""
+}
+variable "subnetwork_id" {
+    default = ""
+}
+
