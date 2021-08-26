312fdf7b72cfe85788d6709bf13f71dee2323faa
beatriz.costa.ter <beatriz.costa.ter@estacio.br>
2021-08-26 17:30:47 -0300

    ajustes de nomenclatura

--- app-teste/android/app/src/main/java/com/apptest/MainActivity.java
+++ app-teste/android/app/src/main/java/com/apptest/MainActivity.java
@@ -1,4 +1,4 @@
-package com.apptest;
+package com.app-testetete;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "apptest";
+    return "app-testetete";
   }
 }
--- app-teste/ios/apptestTests/apptestTests.m
+++ app-teste/ios/apptestTests/apptestTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface apptestTests : XCTestCase
+@interface app-testeTests : XCTestCase
 
 @end
 
-@implementation apptestTests
+@implementation app-testeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
